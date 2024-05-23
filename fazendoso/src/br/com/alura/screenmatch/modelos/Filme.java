af1969a66818e1970f647df38b9188c0f41ccf03
Rogério Pantoja Baena <rogerio_baena@icloud.com>
2024-05-23 20:37:49 -0300

    Add files via upload
    Escreve um programa em Java para lidar com uma lista de títulos, incluindo filmes e séries, e os classifica de várias maneiras. Aqui está um resumo do que está acontecendo no código:
    
    Define algumas instâncias de filmes e uma série.
    Cria uma lista chamada lista que pode conter objetos do tipo Titulo.
    Adiciona os títulos à lista.
    Itera sobre a lista e imprime o nome de cada título. Se o título for um filme com uma classificação maior que 2, você imprime a classificação também.
    Cria uma lista de strings chamada buscaPorArtista e adiciona alguns nomes de artistas a ela.
    Ordena a lista buscaPorArtista em ordem alfabética e imprime-a.
    Ordena a lista lista de títulos e imprime-a.
    Ordena a lista lista de títulos por ano de lançamento e imprime-a.

---
+++ fazendoso/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,36 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+
+    private String diretor;
+
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+
+    @Override
+    public int getClassificacao() {
+        return (int) (pegaMedia() / 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
+    }
+}
